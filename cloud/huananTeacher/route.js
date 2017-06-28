9118e63762c5859cf5d3ba2ac8c52d7492c49218
MQpeng <742486126@qq.com>
2017-06-28 10:54:07 +0800

    update

--- cloud/huananTeacher/route.js
+++ cloud/huananTeacher/route.js
@@ -4,6 +4,7 @@ var ctrl = require('./controller');
 var start = true; // 莫名奇妙会触发两次
 Parse.Cloud.define("data_teacher", (request, response) => {
   console.log("开始", start)
+
   if (start) {
     ctrl.syncTeacher(request, response)
     start = false;
