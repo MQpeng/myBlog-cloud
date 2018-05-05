546a99ae2d90830c2836efe76c8de5ee2655e97d
PengShao <742486126@qq.com>
2018-05-05 16:34:11 +0800

    update ECStream trigger

--- cloud/ECStream/trigger.js
+++ cloud/ECStream/trigger.js
@@ -1,6 +1,6 @@
 'use strict'
 
-Parse.Cloud.beforeSave('Staff', function(request, response) {
+Parse.Cloud.afterSave('Staff', function(request, response) {
   let staffObject = request.object;
   userSignUp(staffObject)
 
@@ -9,10 +9,16 @@ Parse.Cloud.beforeSave('Staff', function(request, response) {
 
 function userSignUp(staffObject){
   let phoneNumber = staffObject.get('staffPhone');
+  let objectId = staffObject.id;
   var user = new Parse.User();
   user.set("username", phoneNumber);
   user.set("password", "123456");
   user.set("phone", phoneNumber);
+  user.set('staff',{
+    "__type": "Pointer",
+    "className": "Staff",
+    "objectId": objectId
+  })
   user.signUp(null, {
     success: function(user) {
       Parse.User.logOut()
